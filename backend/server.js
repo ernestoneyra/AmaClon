3458b19852eb4213ca1bb1c9a47e2177a24ae862
Ernesto Neyra <ernestoneyra31@gmail.com>
2021-06-15 15:56:02 +0200

    publish heroku

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amaclon", {
 });
 
 app.use("/api/uploads", uploadRouter);
+
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
@@ -33,9 +34,14 @@ app.get("/api/config/paypal", (req, res) => {
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.get("/", (req, res) => {
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
+/* app.get("/", (req, res) => {
   res.send("Server is ready");
-});
+}); */
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
